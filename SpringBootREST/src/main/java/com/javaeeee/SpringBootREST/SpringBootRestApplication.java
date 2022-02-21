768bb4fb97f3fc5508c02d16d4786a7c758e37a0
Rulo17 <71820128+Rulo17@users.noreply.github.com>
2022-02-21 09:45:54 +0100

    Bloque6-Rubrica3

---
+++ SpringBootREST/src/main/java/com/javaeeee/SpringBootREST/SpringBootRestApplication.java
@@ -0,0 +1,15 @@
+package com.javaeeee.SpringBootREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.javaeeee.SpringBootREST.*" })
+public class SpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApplication.class, args);
+	}
+
+}
